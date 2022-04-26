261fe86fea9b4ae6d44a71635b7d13fc15278972
Athansya <atoriz98@outlook.com>
2022-04-26 07:02:17 +0000

    Added different tools, configured git and fish shell

--- home.nix
+++ home.nix
@@ -6,11 +6,56 @@
   home.username = "atoriz98";
   home.homeDirectory = "/home/atoriz98";
 
+  home.sessionVariables = {
+     EDITOR = "vim";  # Predefined editor
+  };
+
   # Packages to install
   home.packages = [
      # pkgs is the set of all packages in the default home.nix implementation
+
+     # Terminal
+     pkgs.tmux # Terminal multiplexer
+
+     # Editors
      pkgs.vim
+
+     # Tools
+     pkgs.bat # Pretty cat replacement
+     pkgs.exa # Better ls written in Rust
+     pkgs.tldr # Simpler manpage with examples
   ];
+
+  # Programs to install and configure in a custom way
+  programs = {
+     # Fish Shell
+     fish = {
+        enable = true;
+     
+        # Plugins // Look-up info. in NixOS.org + Github
+        plugins = [{
+           name="foreign-env";
+           src = pkgs.fetchFromGitHub {
+              owner = "oh-my-fish";
+              repo = "plugin-foreign-env";
+              rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+              sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+           };
+        }];
+     };
+     
+     # Git
+     git = {
+        enable = true;
+        userName = "Athansya";
+        userEmail = "atoriz98@outlook.com";
+     };
+  };
+
+
+  # TODO
+  # Configure VIM
+  # 
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
